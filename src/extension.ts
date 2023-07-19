5c47d82ef88c22652cbfd47711e9cb90d74d16c1
Anna Cardon <t-annacardon@microsoft.com>
2023-07-19 16:44:56 -0700

    edited the extension.ts to instead open a blank text doc when activated

--- src/extension.ts
+++ src/extension.ts
@@ -2,26 +2,25 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+	//tell me extension was activated in console logs
 	console.log('Congratulations, your extension "annaPrototype" is now active!');
+    // Register the command
+    const disposable = vscode.commands.registerCommand('annaPrototype.helloWorld', () => {
+        // Create an empty string for the content of the new text document
+        const content = '';
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('annaPrototype.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Prototype!');
-	});
+        // Create a new untitled text document with the specified content
+        vscode.workspace.openTextDocument({ content }).then(doc => {
+            // Show the newly created document in the editor
+            vscode.window.showTextDocument(doc);
+        });
+    });
 
-	context.subscriptions.push(disposable);
+    // Add the command to the context
+    context.subscriptions.push(disposable);
 }
 
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+    // Clean up resources when the extension is deactivated
+}
