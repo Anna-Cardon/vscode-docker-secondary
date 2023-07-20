6733f0adf57dfff569fe83f21b7f34bf224a57f1
Anna Cardon <t-annacardon@microsoft.com>
2023-07-20 16:18:14 -0700

    made the code able to communitcate with the vscode terminal and follow host path to oras

--- src/extension.ts
+++ src/extension.ts
@@ -23,15 +23,36 @@ export function activate(context: vscode.ExtensionContext) {
 
 	//this creates a terminal and names it Ext Terminal #1... then shows a info message based on success
 	context.subscriptions.push(vscode.commands.registerCommand('terminalTest.createTerminal', () => {
+		/**
+		 * Currently the code only works for my host path. Think about the process of asking the user to install oras,
+		 * how we'll get their paths, and how to make the installation seamless if possible.
+		 * Think about how we'll get the output into a var to be printed in our empty text doc
+		 * Look at the docker code to see how it communitcated with CLI because the current code is only interacting with the 
+		 * terminal within vscode. It should be doing that.
+		 */
 		try {
 			const terminal = vscode.window.createTerminal(`Ext Terminal #${NEXT_TERM_ID++}`);
-			terminal.sendText("echo 'Sent text immediately after creating'"); //this puts in a command			
+			//terminal.sendText("cmd"); //this puts in a command	
+			terminal.sendText("cd C:\\Users\\t-annacardon\\oras"); //this puts in a command	
+			//terminal.sendText("windows"); //this puts in a command	
+			terminal.sendText(".\\oras"); //this puts in a command	
+			// terminal.sendText("set VERSION='1.0.0'"); //this puts in a command	
+			// terminal.sendText("curl.exe -sLO  'https://github.com/oras-project/oras/releases/download/v1.0.0/oras_1.0.0_windows_amd64.zip'"); //this puts in a command	
+			// terminal.sendText("tar.exe -xvzf oras_1.0.0_windows_amd64.zip"); //this puts in a command	
+			// terminal.sendText("mkdir -p oras"); //this puts in a command	
+			// terminal.sendText("copy oras.exe oras"); //this puts in a command	
+			// terminal.sendText("set PATH=oras;%PATH%"); //this puts in a command	
+			// terminal.sendText("PATH"); //this puts in a command	
+			
+			terminal.sendText(".\\oras discover -h"); //this puts in a command	
+
 			vscode.window.showInformationMessage('Hello World 2!');
 		} catch (error) {
 			console.error('Failed to create terminal:', error);
 			vscode.window.showErrorMessage('Failed to create terminal. See console for details.');
 		}
 	}));
+
 	//this will show the terminals currently running and if not running it'll say no terminals available
 	context.subscriptions.push(vscode.commands.registerCommand('terminalTest.show', () => {
 		if (ensureTerminalExists()) {
