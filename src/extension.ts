63656776aacfb2f2fb6d56ca6143a11bb670b1c5
Anna Cardon <t-annacardon@microsoft.com>
2023-07-26 14:32:06 -0700

    edited extensions and showreferrer to connect to the interface of docker

--- src/extension.ts
+++ src/extension.ts
@@ -4,25 +4,40 @@ import * as vscode from 'vscode';
 import { showReferrer } from './commands/showReferrer';
 
 
-export function activate(context: vscode.ExtensionContext) {
-	//let NEXT_TERM_ID = 1; //number the term var
-
-	//tell me extension was activated in console logs
-	console.log('Congratulations, your extension "annaPrototype" is now active!');
-    // Register the command
-    const disposable = vscode.commands.registerCommand('annaPrototype.helloWorld', async () => {
-        // empty string for the content of the new text document
-        const content = '';
-        // Create a new untitled text document with the specified content, then..
-        vscode.workspace.openTextDocument({ content }).then(doc => {
-            // Show the newly created document in the editor
-            vscode.window.showTextDocument(doc);
-        });
-		const result = await showReferrer();
-		console.log( 'extension output:' + result);
-
-    });
-
-    // Add the command to the context
-    context.subscriptions.push(disposable);
+interface RemoteTagTreeItem {
+	readonly fullTag: string;
 }
+
+export function activate(context: vscode.ExtensionContext) {
+
+	context.subscriptions.push(vscode.commands.registerCommand('annaPrototype.helloWorld',
+		async (remoteTag: RemoteTagTreeItem) => {
+			const result = await showReferrer(remoteTag);
+			console.log( 'extension output:' + result);
+		}
+	));
+}
+
+// export function activate(context: vscode.ExtensionContext) {
+// 	//let NEXT_TERM_ID = 1; //number the term var
+
+// 	//tell me extension was activated in console logs
+// 	console.log('Congratulations, your extension "annaPrototype" is now active!');
+//     // Register the command
+//     const disposable = vscode.commands.registerCommand('annaPrototype.helloWorld', async (arg1 : unknown) => { //arg1 will have the treeItem and we can access it here arg1.fullTag
+//         // empty string for the content of the new text document
+//         const content = '';
+//         // Create a new untitled text document with the specified content, then..
+//         vscode.workspace.openTextDocument({ content }).then(doc => {
+//             // Show the newly created document in the editor
+//             vscode.window.showTextDocument(doc);
+//         });
+// 		const result = await showReferrer();
+// 		console.log( 'extension output:' + result);
+
+//     });
+
+//     // Add the command to the context
+//     context.subscriptions.push(disposable);
+// }
+
